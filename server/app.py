ecb151760c974ac0fb8758ac6e8b86bdebd9dd57
Kalvine <kkalvine4@gmail.com>
2024-10-08 22:55:24 +0300

    flask restful intro

--- server/app.py
+++ server/app.py
@@ -17,7 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        
+        return make_response(response_dict_list, 200)
+    
+    def post(self):
+        new_record = Newsletter(
+            title = request.form['title'],
+            body = request.form['body']
+        )
+        
+        db.session.add(new_record)
+        db.session.commit()
+        
+        return make_response(new_record.to_dict(), 200)
+        
+api.add_resource(Home,'/newsletters')
 
+class NewsletterById(Resource):
+    
+    def get(self, id):
+        newsletter = Newsletter.query.filter(Newsletter.id==id).first()
+        newsletter_dict = newsletter.to_dict()
+        
+        return make_response(newsletter_dict, 200)
+    
+    def patch(self, id):
+        newsletter = Newsletter.query.filter(Newsletter.id==id).first()
+        
+        for attr in request.form:
+            setattr(newsletter, attr, request.form.get(attr))
+
+            db.session.add(newsletter)
+            db.session.commit()
+            
+            return make_response(newsletter.to_dict(), 200)
+        
+    def delete(self, id):
+        newsletter = Newsletter.query.filter(Newsletter.id==id).first()
+        
+        db.session.delete(newsletter)
+        db.session.commit()
+    
+api.add_resource(NewsletterById, '/newsletters/<int:id>')
+
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
